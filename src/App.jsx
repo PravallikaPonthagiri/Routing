9ba6cc5c61548b71a3fa8ab36e169174e08d8c01
PravallikaPonthagiri <ponthagiripravallika>
2026-02-11 10:24:02 +0530

    This is my project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Login from "./Components/Login"
+import Home from "./Components/Home"
+import Dashboard from "./Components/Dashboard"
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
+}
+
